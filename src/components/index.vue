23b9f0cc72171b9ffceccd8690ae182096681152
Huz0752 <ja19991205@gmail.com>
2024-10-26 02:12:53 +0800

    feat: 確定footer模板
    1. 確定分為履歷/Email/社群媒體三塊
    2. 下方增設最後更新時間
    
    issue #2

--- src/components/index.vue
+++ src/components/index.vue
@@ -240,85 +240,34 @@
         <div id="footer">
             <div class="container">
                 <div class="row">
-                    <div class="3u">
+                    <div class="4u">
                         <section>
-                            <h2>Maecenas lectus</h2>
-                            <div class="balloon">
-                                <blockquote>“  Donec leo, vivamus ullamcorper fermentum nibh in augue pulvinar
-                                    ullamcorper
-                                    metus praesent a lacus at urna congue ullamcorper rutrum.  ”<br><br><strong>–  John
-                                        Smith</strong></blockquote>
-                            </div>
-                            <div class="ballon-bgbtm"> </div>
+                            <h2>{{ $t("footer.resume") }}</h2>
+                            <i class="fa-solid fa-file-user"></i>
+                            <p><strong>Huz07522@gmail.com </strong></p>
+                            <!-- <a href="#" class="button">Submit</a> -->
+                             <br>
                         </section>
                     </div>
-                    <div class="3u">
+                    <div class="4u">
                         <section>
-                            <h2>Donec dictum</h2>
-                            <ul class="default">
-                                <li>
-                                    <h3>Mauris vulputate dolor sit amet</h3>
-                                    <p><a href="#">Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna
-                                            congue rutrum.</a></p>
-                                </li>
-                                <li>
-                                    <h3>Fusce ultrices fringilla metus</h3>
-                                    <p><a href="#">Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna
-                                            congue rutrum.</a></p>
-                                </li>
-                                <li>
-                                    <h3>Donec dictum metus in sapien</h3>
-                                    <p><a href="#">Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna
-                                            congue rutrum.</a></p>
-                                </li>
-                            </ul>
+                            <h2>{{ $t("footer.contact") }}</h2>
+                            <p><strong>Huz07522@gmail.com </strong></p>
+                            <!-- <a href="#" class="button">Submit</a> -->
+                             <br>
                         </section>
                     </div>
-                    <div class="3u">
+                    <div class="4u">
                         <section>
-                            <h2>Nulla leifend</h2>
-                            <p>Donec placerat odio vel elit. Nullam ante orci, pellentesque eget, tempus quis, ultrices
-                                in,
-                                est. Curabitur sit amet nulla. Nam in massa. Sed vel tellus. Curabitur sem urna,
-                                consequat.
-                            </p>
-                            <ul class="style5">
-                                <li><a href="#"><img src="../assets/images/pics07.jpg" alt="" width="80"
-                                            height="80"></a>
-                                </li>
-                                <li><a href="#"><img src="../assets/images/pics08.jpg" alt="" width="80"
-                                            height="80"></a>
-                                </li>
-                                <li><a href="#"><img src="../assets/images/pics09.jpg" alt="" width="80"
-                                            height="80"></a>
-                                </li>
-                                <li><a href="#"><img src="../assets/images/pics10.jpg" alt="" width="80"
-                                            height="80"></a>
-                                </li>
-                                <li><a href="#"><img src="../assets/images/pics11.jpg" alt="" width="80"
-                                            height="80"></a>
-                                </li>
-                                <li><a href="#"><img src="../assets/images/pics12.jpg" alt="" width="80"
-                                            height="80"></a>
-                                </li>
-                            </ul><a href="#" class="button">More Collections</a>
+                            <h2>{{ $t("footer.social") }}</h2>
+                            <p><strong>Huz07522@gmail.com </strong></p>
+                            <!-- <a href="#" class="button">Submit</a> -->
+                             <br>
                         </section>
                     </div>
-                    <div class="3u">
-                        <section>
-                            <h2>Luctus eleifend</h2>
-                            <p><strong>Aliquam erat volutpat. Pellentesque tristique ante ut risus. </strong></p>
-                            <p>Quisque dictum. Integer nisl risus, sagittis convallis, rutrum id, elementum congue,
-                                nibh.
-                                Suspendisse dictum porta lectus. Donec placerat odio vel elit.</p>
-                            <p>Donec placerat odio vel elit. Nullam ante orci, pellentesque eget, tempus quis, ultrices
-                                in,
-                                est. Curabitur sit amet nulla. Nam in massa. Sed vel tellus. Curabitur sem urna,
-                                consequat.
-                            </p>
-                            <a href="#" class="button">More Collections</a>
-                        </section>
-                    </div>
+                </div>
+                <div id="lastUpdate">
+                    <p>{{ $t("footer.lastUpdate") }}</p>
                 </div>
             </div>
         </div>
--- src/main.js
+++ src/main.js
@@ -10,13 +10,25 @@ const i18n = createI18n({
             name: "WU JUN-YI",
             welcome: "Welcome!",
             title: "WU JUN-YI's personal website",
-            navbar: ["Homepage", "Left Sidebar", "Right Sidebar", "CONTACT ME"]
+            navbar: ["Homepage", "Left Sidebar", "Right Sidebar", "CONTACT ME"],
+            footer: {
+                contact: "Contact Me",
+                lastUpdate: "Last Update: 2024.10.26",
+                social: "Social Media",
+                resume: "Resume",
+            }
         },
         zh: {
             name: "伍竣義",
             welcome: "歡迎!",
             title: "伍竣義的個人網站",
-            navbar: ["主頁", "測試", "測試2", "聯絡我"]
+            navbar: ["主頁", "測試", "測試2", "聯絡我"],
+            footer: {
+                contact: "聯絡我",
+                lastUpdate: "最近更新: 2024.10.26",
+                social: "社群媒體",
+                resume: "履歷",
+            }
         },
     },
 });
